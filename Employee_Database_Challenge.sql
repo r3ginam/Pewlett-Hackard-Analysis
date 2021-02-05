3e38e07357dad3a77a04e363f2df3679ae49d1c3
Regina Manion <72927699+r3ginam@users.noreply.github.com>
2021-02-05 13:39:52 -0600

    parts 1 and 2 of challenge completed

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,32 @@
+COMMIT
+
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
